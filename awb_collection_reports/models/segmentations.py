bb34e54c8f46d475380d1f0194dee2e01673efe1
HughSonobavich <baroniaryan@gmail.com>
2021-07-26 16:35:10 +0800

    #475 enhancements/Invoice Segmentations

--- awb_collection_reports/models/segmentations.py
+++ awb_collection_reports/models/segmentations.py
@@ -1,5 +1,9 @@
 from odoo import api, fields, models, _
 
+import logging
+
+
+_logger = logging.getLogger(__name__)
 
 class AWBSegmentationInvoice(models.Model):
 	_inherit = 'account.move'
@@ -8,21 +12,39 @@ class AWBSegmentationInvoice(models.Model):
 	device = fields.Float()
 	security_deposit = fields.Float()
 	others = fields.Float()
+	segmentation_price = fields.Monetary(related='invoice_line_ids.price_subtotal')
+	segmentation_boolean = fields.Boolean(compute='segmentation_recompute')
 
-	@api.onchange('invoice_line_ids.product_id')
-	def compute_segmentations(self):
-
+	@api.depends('segmentation_boolean')
+	def segmentation_recompute(self):
 		for rec in self:
-			if rec.invoice_line_ids:
-				for line in self.invoice_line_ids:
-					if line.product_id.product_tmpl_id.product_segmentation == 'month_service':
-						self.monthly_service = line.price_unit * line.quantity
-					elif line.product_id.product_tmpl_id.product_segmentation == 'device':
-						self.device = line.price_unit * line.quantity
-					elif line.product_id.product_tmpl_id.product_segmentation == 'security_deposit':
-						self.security_deposit = line.price_unit * line.quantity
-					elif line.product_id.product_tmpl_id.product_segmentation == 'others':
-						self.others = line.price_unit * line.quantity
+			rec.segmentation_boolean = True
+			self.compute_segmentations()
+
+	def compute_segmentations(self):
+		total1 = 0
+		total2 = 0
+		total3 = 0
+		total4 = 0
+		if self.invoice_line_ids:
+			for rec in self.invoice_line_ids:
+				if rec.product_id.product_tmpl_id.product_segmentation == "month_service":
+					total1 += rec.price_subtotal
+					self.monthly_service = total1
+					continue
+				elif rec.product_id.product_tmpl_id.product_segmentation == "device":
+					total2 += rec.price_subtotal
+					self.device = total2
+					continue
+				elif rec.product_id.product_tmpl_id.product_segmentation == "security_deposit":
+					total3 += rec.price_subtotal
+					self.security_deposit = total3
+					continue
+				elif rec.product_id.product_tmpl_id.product_segmentation == "others":
+					total4 += rec.price_subtotal
+					self.others = total4
+					continue
+				
 
 class AWBSegmentationPayment(models.Model):
 	_inherit = 'account.payment'
@@ -31,3 +53,36 @@ class AWBSegmentationPayment(models.Model):
 	device = fields.Float()
 	security_deposit = fields.Float()
 	others = fields.Float()
+
+	segmentation_price = fields.Monetary(related='invoice_line.invoice_amount')
+	segmentation_boolean = fields.Boolean(compute='segmentation_recompute')
+
+	# @api.depends('segmentation_boolean')
+	# def segmentation_recompute(self):
+	# 	for rec in self:
+	# 		rec.segmentation_boolean = True
+	# 		self.compute_segmentations_payments()
+
+	# def compute_segmentations_payments(self):
+	# 	total1 = 0
+	# 	total2 = 0
+	# 	total3 = 0
+	# 	total4 = 0
+	# 	if self.invoice_line:
+	# 		for rec in self.invoice_line:
+	# 			if rec.invoice.invoice_line_ids.product_id.product_tmpl_id.product_segmentation == "month_service":
+	# 				total1 += rec.invoice_amount
+	# 				self.monthly_service = total1
+	# 				continue
+	# 			elif rec.invoice.invoice_line_ids.product_id.product_tmpl_id.product_segmentation == "device":
+	# 				total2 += rec.invoice_amount
+	# 				self.device = total2
+	# 				continue
+	# 			elif rec.invoice.invoice_line_ids.product_id.product_tmpl_id.product_segmentation == "security_deposit":
+	# 				total3 += rec.invoice_amount
+	# 				self.security_deposit = total3
+	# 				continue
+	# 			elif rec.invoice.invoice_line_ids.product_id.product_tmpl_id.product_segmentation == "others":
+	# 				total4 += rec.invoice_amount
+	# 				self.others = total4
+	# 				continue
