9ddde85ab3c606f42730c9f33c51febb4f92164c
HughSonobavich <baroniaryan@gmail.com>
2021-07-26 16:51:22 +0800

    #484 enhancements/Payment Segmentations

--- awb_collection_reports/models/segmentations.py
+++ awb_collection_reports/models/segmentations.py
@@ -55,34 +55,36 @@ class AWBSegmentationPayment(models.Model):
 	others = fields.Float()
 
 	segmentation_price = fields.Monetary(related='invoice_line.invoice_amount')
-	segmentation_boolean = fields.Boolean()#compute='segmentation_recompute')
+	segmentation_boolean = fields.Boolean(compute='segmentation_recompute')
 
-	# @api.depends('segmentation_boolean')
-	# def segmentation_recompute(self):
-	# 	for rec in self:
-	# 		rec.segmentation_boolean = True
-	# 		self.compute_segmentations_payments()
+	@api.depends('segmentation_boolean')
+	def segmentation_recompute(self):
+		for rec in self:
+			rec.segmentation_boolean = True
+			self.compute_segmentations_payments()
 
-	# def compute_segmentations_payments(self):
-	# 	total1 = 0
-	# 	total2 = 0
-	# 	total3 = 0
-	# 	total4 = 0
-	# 	if self.invoice_line:
-	# 		for rec in self.invoice_line:
-	# 			if rec.invoice.invoice_line_ids.product_id.product_tmpl_id.product_segmentation == "month_service":
-	# 				total1 += rec.invoice_amount
-	# 				self.monthly_service = total1
-	# 				continue
-	# 			elif rec.invoice.invoice_line_ids.product_id.product_tmpl_id.product_segmentation == "device":
-	# 				total2 += rec.invoice_amount
-	# 				self.device = total2
-	# 				continue
-	# 			elif rec.invoice.invoice_line_ids.product_id.product_tmpl_id.product_segmentation == "security_deposit":
-	# 				total3 += rec.invoice_amount
-	# 				self.security_deposit = total3
-	# 				continue
-	# 			elif rec.invoice.invoice_line_ids.product_id.product_tmpl_id.product_segmentation == "others":
-	# 				total4 += rec.invoice_amount
-	# 				self.others = total4
-	# 				continue
+	def compute_segmentations_payments(self):
+		total1 = 0
+		total2 = 0
+		total3 = 0
+		total4 = 0
+		if self.invoice_line:
+			for rec in self.invoice_line:
+				for line in rec.invoice:
+					for x in line.invoice_line_ids:
+						if x.product_id.product_tmpl_id.product_segmentation == "month_service":
+							total1 += x.price_subtotal
+							self.monthly_service = total1
+							continue
+						elif x.product_id.product_tmpl_id.product_segmentation == "device":
+							total2 += x.price_subtotal
+							self.device = total2
+							continue
+						elif x.product_id.product_tmpl_id.product_segmentation == "security_deposit":
+							total3 += x.price_subtotal
+							self.security_deposit = total3
+							continue
+						elif x.product_id.product_tmpl_id.product_segmentation == "others":
+							total4 += x.price_subtotal
+							self.others = total4
+							continue
